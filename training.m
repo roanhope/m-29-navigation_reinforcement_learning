78f3736107263378198d6844bf3a292ae18fe3b4
cuezee <squarecely@yahoo.com>
2020-08-08 18:58:21 +0800

    cleaning errors

--- training.m
+++ training.m
@@ -1,13 +1,49 @@
+%% Parameters for C23-L33-W20 terminal voltage for this motor is 12V
+La= 0.94e-3; %H, inductance of the armature winding
+Ra= 1; %Ohm, resistance of the armature winding
+Kb=  0.0301; %V/rad/s the back emf constant 
+
+N=10;  %the gear ratio
+Kt=0.03; %Nm/Amp the torque constant
+
+%% Parameters for robot body
+M = 1000e-3; % Robot mass in KG
+d = 0.01;%0.0; % Location of center of gravity of robot. x=d y=o in robot frame
+R = 0.02;%radius of wheel in meter
+L = 20e-2;%distance between wheel in meter
+J = 0.01;% Kg*m^2 Moment inertia of disk robot KG*m^2 calculated using https://www.omnicalculator.com/physics/mass-moment-of-inertia
+
+%% gain
+k_eh = 1;           % S = e_h_dot + k_eh*e_h
+k_ep = 1;
+kseh = 40;          %S scaler
+kseh_dot = 5e4;     %S dot scaler
+ksep = 1;           %S scaler
+ksep_dot = 100;     %S dot scaler
+
+%% Input
+tau_b = 0; %disturbance amplitude
+f = 10;
+
+G_x = -5; %When not using path generator
+G_y = 0;  %When not using path generator
+
+xa_init=0;
+ya_init=0;
+theta_init=deg2rad(0);
+
+vol_gain = 1; % to slowdown the robot
+
 %% Create Environment Interface
 % Creating an environment model includes defining the following:
 % 1. Action and observation signals that the agent uses to interact with the environment. For more information, see rlNumericSpec and rlFiniteSetSpec.
 % 2. Reward signal that the agent uses to measure its success. For more information, see Define Reward Signals.
 % Define the observation specification obsInfo and action specification actInfo.
-obsInfo = rlNumericSpec([4 1],... %integral error, error, height
-    'LowerLimit',[-inf -inf -inf -inf]',...
-    'UpperLimit',[ inf  inf  inf  inf]');
+obsInfo = rlNumericSpec([2 1],... %integral error, error, height
+    'LowerLimit',[-inf -inf]',...
+    'UpperLimit',[ inf  inf]');
 obsInfo.Name = 'observations';
-obsInfo.Description = 's_pos, s_pos_dot, s_hed, and s_hed_dot';
+obsInfo.Description = 'distance error and heading error';
 numObservations = obsInfo.Dimension(1);
 
 actInfo = rlNumericSpec([2 1]);
@@ -64,8 +100,8 @@ criticNetwork = connectLayers(criticNetwork,'CriticStateFC2','add/in1');
 criticNetwork = connectLayers(criticNetwork,'CriticActionFC1','add/in2');
 
 %% View the critic network configuration.
-figure
-plot(criticNetwork)
+% figure
+% plot(criticNetwork)
 
 %% Specify options for the critic representation using "rlRepresentationOptions".
 criticOpts = rlRepresentationOptions('LearnRate',1e-03,'GradientThreshold',1);
@@ -148,12 +184,12 @@ experiences = sim(env,agent,simOpts);
 %% Local Function
 function in = localResetFcn(in)
     % randomize G_x
-    blk = sprintf('moel/G_x');
-    G_x = randn*10;
+    blk = sprintf('model/G_x');
+    G_x = randn*5;
     in = setBlockParameter(in,blk,'Value',num2str(G_x));
     
     % randomize G_y
-    blk = sprintf('moel/G_y');
-    G_y = randn*10;
+    blk = sprintf('model/G_y');
+    G_y = randn*5;
     in = setBlockParameter(in,blk,'Value',num2str(G_y));
 end
